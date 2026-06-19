f75428be02c1660e4ac7b99997d4712f3c8c47ee
mariaclarardrges-maker <mpilar.ribeiro20@gmail.com>
2026-06-19 08:00:39 -0300

    aaa

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
-   private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            Pontuacao pontuacao = other.GetComponent<Pontuacao>();
+            pontuacao.AddPoints();
             Destroy(this.gameObject);
         }
     }
